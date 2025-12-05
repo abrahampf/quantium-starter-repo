419ca473278338eeca4abf90cf74c784f4966aca
abrahampasillas <abrahamp@uw.edu>
2025-12-04 23:00:30 -0800

    ci testing

---
+++ src/data_processing/conftest.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.chrome.options import Options
+
+def pytest_setup_options():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    return options
